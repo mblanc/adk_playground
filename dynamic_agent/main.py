d9aeb4947da20186f707ac0105dca899cefbf282
Matthieu Blanc <mblanc@google.com>
2025-08-04 09:29:44 +0000

    Dynamic agent

--- dynamic_agent/main.py
+++ dynamic_agent/main.py
@@ -1,26 +1,40 @@
-import os
+import vertexai
+from vertexai import agent_engines
+from vertexai.preview import reasoning_engines
+from google.adk.agents import Agent
+from google.adk.tools import FunctionTool, ToolContext
+from google.genai import Client, types
+from pydantic import BaseModel
+from typing import List
+from typing import Optional
+import uuid
 import time
 
-from google.adk.agents import Agent
-from google.adk.planners import BuiltInPlanner
-from google.adk.tools import FunctionTool, ToolContext, load_artifacts
-from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, SseConnectionParams
-from google.genai import Client, types
-import uuid
 
 MODEL = "gemini-2.5-flash"
 MODEL_IMAGE = "imagen-4.0-fast-generate-preview-06-06"
 MODEL_VIDEO = "veo-2.0-generate-001"
 
-client = Client(
-    vertexai=True,
-    project=os.getenv("GOOGLE_CLOUD_PROJECT"),
-    location=os.getenv("GOOGLE_CLOUD_LOCATION"),
-)
+PROJECT_ID = "svc-demo-vertex"
+LOCATION = "us-central1"
+STAGING_BUCKET = "gs://svc-demo-vertex-us"
+
+class AgentSpec(BaseModel):
+    name: str
+    model: str
+    description: str
+    instruction: str
+    tools: Optional[List[str]] = None
+    sub_agents: Optional[List[str]] = None
 
 
 async def generate_image(tool_context: "ToolContext", img_prompt: str, aspect_ratio: str = "16:9"):
     """Generates an image based on the prompt."""
+    client = Client(
+        vertexai=True,
+        project=PROJECT_ID,
+        location=LOCATION,
+    )
     print("#"*27)
     print(img_prompt)
     response = client.models.generate_images(
@@ -52,6 +66,11 @@ async def generate_image(tool_context: "ToolContext", img_prompt: str, aspect_ra
 
 async def modify_image(tool_context: "ToolContext", img_prompt: str, image_filename: str, aspect_ratio: str = "16:9"):
     """Modify an image based on the modified prompt."""
+    client = Client(
+        vertexai=True,
+        project=PROJECT_ID,
+        location=LOCATION,
+    )
     response = client.models.generate_images(
         model=MODEL_IMAGE,
         prompt=img_prompt,
@@ -80,6 +99,11 @@ async def modify_image(tool_context: "ToolContext", img_prompt: str, image_filen
 
 async def generate_video(tool_context: "ToolContext", video_prompt: str, image_filename: str, aspect_ratio: str = "16:9"):
     """Generates a video based on the prompt and image."""
+    client = Client(
+        vertexai=True,
+        project=PROJECT_ID,
+        location=LOCATION,
+    )
     image_artifact = await tool_context.load_artifact(image_filename)
     if image_artifact and image_artifact.inline_data:
         print(f"Successfully loaded latest Python artifact '{image_filename}'.")
@@ -122,29 +146,78 @@ async def generate_video(tool_context: "ToolContext", video_prompt: str, image_f
             "status": "error",
             "detail": "Image not found."
         }
-    
 
-generate_image_tool = FunctionTool(func=generate_image)
-generate_video_tool = FunctionTool(func=generate_video)
-modify_image_tool = FunctionTool(func=modify_image)
-# mcp_toolset = MCPToolset(
-#     connection_params=SseConnectionParams(url="http://localhost:8000/mcp"),
-# )
 
-root_agent = Agent(
-    name="media_agent",
-    model=MODEL,
-    planner=BuiltInPlanner(
-        thinking_config=types.ThinkingConfig(
-            include_thoughts=False,
-        )
-    ),
-    description=("Media agent able to generate images and videos."),
-    instruction=(
-        "You are a helpful agent who can answer user questions and generate images."
-        "When user ask you to generate an image, always generate images using the generate_image tool."
-        "When user ask you to generate a video, always generate an image first using the generate_image tool."
-        "Then use the generate_video tool to generate a video from the image."
-    ),
-    tools=[generate_image_tool, generate_video_tool, load_artifacts],
-)
+tool_map = {
+    "generate_image": FunctionTool(func=generate_image),
+    "generate_video": FunctionTool(func=generate_video)
+}
+
+def create_agent_from_spec(agent_spec: AgentSpec) -> Agent:
+    agent = Agent(
+        name=agent_spec.name,
+        model=agent_spec.model,
+        description=agent_spec.description,
+        instruction=agent_spec.instruction,
+        tools=[tool_map[tool] for tool in agent_spec.tools],
+    )
+    return agent
+
+
+
+ 
+print("I am fine")
+ 
+if __name__ == "__main__":
+    agent_spec = {
+        "name": "",
+    }
+
+    agent_spec = AgentSpec(
+        name="media_agent",
+        model="gemini-2.5-flash",
+        description=("Media agent able to generate images and videos."),
+        instruction=(
+            "You are a helpful agent who can answer user questions and generate images."
+            "When user ask you to generate an image, always generate images using the generate_image tool."
+            "When user ask you to generate a video, always generate an image first using the generate_image tool."
+            "Then use the generate_video tool to generate a video from the image."
+        ),
+        tools=["generate_image", "generate_video"],
+    )
+
+    print("Create an agent dynamically")
+    agent = create_agent_from_spec(agent_spec)
+    print(agent)
+    vertexai.init(
+        project=PROJECT_ID,
+        location=LOCATION,
+        staging_bucket=STAGING_BUCKET,
+    )
+
+
+    print("Create ADK app")
+    app = reasoning_engines.AdkApp(
+        agent=agent,
+        enable_tracing=True,
+        session_service_builder=None,
+        artifact_service_builder=None,
+        memory_service_builder=None,
+        env_vars={},
+    )
+    print(app)
+
+    print("Deploy to Agent Engine")
+    remote_app = agent_engines.create(
+        agent_engine=app,
+        requirements=[
+            "google-cloud-aiplatform[adk,agent_engines]",
+        ],
+        display_name="Dynamic agent",
+        description="Dynamic agent creation test",
+        gcs_dir_name=None,
+        extra_packages=None,
+        env_vars=None,
+        build_options=None,
+    )
+    print(remote_app)
